1e48abc9721fdac9ed77df8fa62e1ab07a98ffec
David Murphy <32233290+dmrph@users.noreply.github.com>
2024-09-28 13:04:34 -0400

    removed icon

--- app.config.js
+++ app.config.js
@@ -4,13 +4,7 @@ module.exports = {
       slug: "DecibelRecorderApp",
       version: "1.0.0",
       orientation: "portrait",
-      icon: "./assets/icon.png",
       userInterfaceStyle: "light",
-      splash: {
-        image: "./assets/splash.png",
-        resizeMode: "contain",
-        backgroundColor: "#ffffff"
-      },
       assetBundlePatterns: [
         "**/*"
       ],
@@ -19,12 +13,8 @@ module.exports = {
       },
       android: {
         adaptiveIcon: {
-          foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#ffffff"
         }
-      },
-      web: {
-        favicon: "./assets/favicon.png"
       },
       plugins: [
         [
