4f2508c4ccd0b206eb767be7348843e02b1d1e0d
David Murphy <32233290+dmrph@users.noreply.github.com>
2024-12-12 21:39:14 -0500

    changed home page desc

--- Frontend/src/Components/HomePage/HomePage.jsx
+++ Frontend/src/Components/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
         <div className="content">
           <div className="info">
             <h1>Decibel Detector and Sound Mapper! </h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta labore rerum commodi architecto doloribus. In fuga odio ad aliquam recusandae accusantium adipisci, ut, incidunt neque iure exercitationem assumenda totam dolorum?</p>
+            <p>Discover the power of technology to monitor and map noise levels in your area. Our application provides real-time decibel data to help you identify and avoid noise pollution hotspots. Whether you’re looking to find quieter spaces or contribute to a growing database of sound levels, Decibel Detect makes it easy to record, upload, and visualize noise data on an interactive map.</p>
             <a href="#" className="btn">Join Us!</a>
           </div>
           
